de55a8dc15d0d75536604741c34daa2b7281e5ba
Claudia Torreiro <143898254+claudiatorreiro@users.noreply.github.com>
2025-12-19 00:45:19 +0000

    finalização do tutorial django girls

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,19 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+
 from .models import Post
+from .forms import PostForm
+
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()
+    ).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def about(request):
     return render(request, 'blog/about.html')
 
-from django.shortcuts import get_object_or_404
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -29,7 +49,5 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
-from django.shortcuts import render
-
-def post_new(request):
-    return render(request, 'blog/post_edit.html', {})
+def certificados(request):
+    return render(request, 'blog/certificados.html', {})
